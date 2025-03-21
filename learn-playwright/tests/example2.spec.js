3ed7014d8e8d2c49b17d5453a6db0c19644dc942
rosygit-creator <rosy.github@gmail.com>
2025-03-20 17:42:30 -0700

    pw

---
+++ learn-playwright/tests/example2.spec.js
@@ -0,0 +1,11 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://www.google.com');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('Google');
+});
+
+
