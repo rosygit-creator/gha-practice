8039fc8d3245a647c7dd1ec408d78378658c5a8e
rosygit-creator <rosy.github@gmail.com>
2025-03-25 17:35:50 -0700

    add docker

--- learn-playwright/tests/example3.spec.js
+++ learn-playwright/tests/example3.spec.js
@@ -1,12 +1,20 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has title-google', async ({ page }) => {
   await page.goto('https://www.google.com');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('Google');
 });
+
+test('has title-yahoo', async ({ page }) => {
+  await page.goto('https://www.facebook.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('Facebook - log in or sign up');
+});
+
 
 
 test('get started link', async ({ page }) => {
