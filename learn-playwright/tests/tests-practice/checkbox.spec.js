0918c1d0d9dd6a90fc540ecf4c8d3ce7f65ebaa2
rosygit-creator <rosy.github@gmail.com>
2025-08-08 20:43:06 -0700

    add tests

--- learn-playwright/tests/tests-practice/checkbox.spec.js
+++ learn-playwright/tests/tests-practice/checkbox.spec.js
@@ -3,19 +3,13 @@ import { test, expect } from '@playwright/test';
 
 
 test('login', async ({ page }) => {
-  await page.goto('https://practice-automation.com/form-fields/');
+  await page.goto('https://playwright.dev/');
+await page.getByRole('link', { name: 'started' }).click();
 
-  // Click radio button - Red.
-  await page.locator('#drink2').click();
-
-  // verify its cheked
-  await expect(page.locator('#drink2')).toBeChecked();
-
-  // count radio buttons
-  const radioButtonsCount = await page.locator('input[type="checkbox"]').count();
-  expect(radioButtonsCount).toBe(5);
 
   
+  
+
 
 
 
